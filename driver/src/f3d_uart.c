cc1322fcd3790111fb4a22d21dd32d39a5a93f09
Ian Polito <Ian-Polito@users.noreply.github.com>
2019-09-05 15:29:22 +0000

    Add files via upload

--- driver/src/f3d_uart.c
+++ driver/src/f3d_uart.c
@@ -2,67 +2,123 @@
  *f3d_uart.c 
  *contains the initialization basic i/o functions for UART
 ****************************/ 
-
+/* Author: Colin Curry | colcurry
+ * Partner: Ian Polito | ipolito
+ * Class: C335
+ * Date: 9/14/18
+*/
 /* Code: */
 
 #include <stm32f30x.h>
 #include <f3d_uart.h>
+#include <queue.h>
 //the initialization function to call
+queue_t rxbuf;
+queue_t txbuf;
+
 void f3d_uart_init(void) {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-	
-	//initialize TX Pin
-	GPIO_StructInit(&GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOC,&GPIO_InitStructure);
-	
-	//initialize RX Pin
-	GPIO_StructInit(&GPIO_InitStructure);
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-        GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-        GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-        GPIO_Init(GPIOC,&GPIO_InitStructure);
-	
-	//alternate function configuration
-	GPIO_PinAFConfig(GPIOC,4,GPIO_AF_7);
-	GPIO_PinAFConfig(GPIOC,5,GPIO_AF_7);
-	
-	//initialize UART
-	USART_InitTypeDef USART_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	
-	USART_StructInit(&USART_InitStructure);
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART1 ,&USART_InitStructure);
-	USART_Cmd(USART1 , ENABLE);
-	
+  GPIO_InitTypeDef GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC,&GPIO_InitStructure);
+
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC , &GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOC,4,GPIO_AF_7);
+  GPIO_PinAFConfig(GPIOC,5,GPIO_AF_7);
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+  USART_StructInit(&USART_InitStructure);
+  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_Init(USART1 ,&USART_InitStructure);
+  USART_Cmd(USART1 , ENABLE);
+
+  // Initialize the rx and tx queues
+  
+  init_queue(&rxbuf);
+  init_queue(&txbuf);
+
+  // Setup the NVIC priority and subpriority
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x08;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x08;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  // Enable the RX interrupt
+  USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+}
+
+static int TxPrimed = 0;
+int RxOverflow = 0;
+
+void USART1_IRQHandler(void) {
+  if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE)) {
+    int ch;
+    ch = USART_ReceiveData(USART1) & 0xff;
+    if (!enqueue(&rxbuf,ch)) {
+      RxOverflow = 1;
+    }    // overflow case --
+    // throw away data and perhaps flag status
+  }
+  if (USART_GetFlagStatus(USART1,USART_FLAG_TXE)) {
+    int ch;
+    if (dequeue(&txbuf, &ch)) {
+      USART_SendData(USART1,ch);
+    }
+    else {                        // Queue is empty, disable interrupt
+      USART_ITConfig(USART1,USART_IT_TXE,DISABLE);
+      TxPrimed = 0;               // signal putchar to reenable
+    }
+  }
 }
 //sends a character
 int putchar(int c) {
-	while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == (uint16_t)RESET);
-	USART_SendData(USART1, c);
-	return 0;
+  //	while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == (uint16_t)RESET);
+  while(!enqueue(&txbuf, c));
+  if(!TxPrimed) {
+    TxPrimed = 1;
+    flush_uart();
+  }
 } 
 //gets a character
 int getchar(void) {
-	while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == (uint16_t)RESET);
-	int c = USART_ReceiveData(USART1);
-	return c;
+  int c;
+  //	while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == (uint16_t)RESET);
+  //	c = USART_ReceiveData(USART1);
+  while(!dequeue(&rxbuf, &c));
+  return c;
 }
 //sends a string
 void putstring(char *s) {
-	char c = *s;
-	while (c != '\0') {
-		putchar(c);
-		c = *(s++);
-	}
+  char c = *s;
+  while(c != '\0') {
+    putchar(c);
+    c = *(s++);
+  }
 }
+
+void flush_uart(void) {
+  USART_ITConfig(USART1,USART_IT_TXE,ENABLE);
+}
+
+
+
 /* f3d_uart.c ends here */
